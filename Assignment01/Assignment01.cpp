f47598760385c8c0226a0d845a006c678b22649d
TenshiRO <virtineaurum@gmail.com>
2019-09-16 23:15:13 +0900

    Nearest Neighbor Completed

--- Assignment01/Assignment01.cpp
+++ Assignment01/Assignment01.cpp
@@ -23,28 +23,28 @@ int main()
 	char key;
 	scanf("%c", &key);
 
-	FILE *F_I = fopen("monkey.raw", "rb");
-	FILE *F_O = fopen("monkey_out.raw", "wb");
+	FILE* F_I = fopen("monkey.raw", "rb");
+	FILE* F_O = fopen("monkey_out.raw", "wb");
 
-	BYTE* MemIn = new BYTE[height*width];
+	BYTE* MemIn = new BYTE[height * width];
 	BYTE* MemOut;
-	fread(MemIn, sizeof(BYTE), height*width, F_I);
+	fread(MemIn, sizeof(BYTE), height * width, F_I);
 
 	switch (key)
 	{
-	case 'C': 
+	case 'C':
 		printf("Inverse\n");
-		MemOut = new BYTE[height*width];
+		MemOut = new BYTE[height * width];
 		Image_Negative(MemIn, MemOut);
 		break;
 	case 'M':
 		printf("Mirroring\n");
-		MemOut = new BYTE[height*width];
+		MemOut = new BYTE[height * width];
 		Image_Mirroring(MemIn, MemOut);
 		break;
 	case 'F':
 		printf("Flipping\n");
-		MemOut = new BYTE[height*width];
+		MemOut = new BYTE[height * width];
 		Image_Flipping(MemIn, MemOut);
 		break;
 	case 'R':
@@ -52,7 +52,7 @@ int main()
 		printf("Press downsampling factor (e.g. 2): ");
 		scanf("%d", &size_factor);
 
-		MemOut = new BYTE[height*width*size_factor*size_factor];
+		MemOut = new BYTE[height * width * size_factor * size_factor];
 		Image_Reducing(MemIn, MemOut, size_factor);
 		break;
 	case 'N':
@@ -60,7 +60,7 @@ int main()
 		printf("Press upsampling factor (e.g. 2): ");
 		scanf("%d", &size_factor);
 
-		MemOut = new BYTE[height*width*size_factor*size_factor];
+		MemOut = new BYTE[height * width * size_factor * size_factor];
 		Nearest_Neighbor(MemIn, MemOut, size_factor);
 		break;
 	case 'B':
@@ -68,13 +68,13 @@ int main()
 		printf("Press upsampling factor (e.g. 2): ");
 		scanf("%d", &size_factor);
 
-		MemOut = new BYTE[height*width*size_factor*size_factor];
+		MemOut = new BYTE[height * width * size_factor * size_factor];
 		Bilinear_Interpolation(MemIn, MemOut, size_factor);
 		break;
 	}
 
-	fwrite(MemOut, sizeof(BYTE), height*width*size_factor*size_factor, F_O);
-	
+	fwrite(MemOut, sizeof(BYTE), height * width * size_factor * size_factor, F_O);
+
 	delete MemIn;
 	delete MemOut;
 	fclose(F_I);
@@ -86,11 +86,11 @@ int main()
 void Image_Negative(BYTE* MemIn, BYTE* MemOut)
 {
 
-	for(int i = 0; i < height; i++ )
+	for (int i = 0; i < height; i++)
 	{
-		for(int j = 0; j < width; j++ )
+		for (int j = 0; j < width; j++)
 		{
-			MemOut[i*width+j] = 255-MemIn[i*width+j];
+			MemOut[i * width + j] = 255 - MemIn[i * width + j];
 		}
 	}
 }
@@ -101,10 +101,10 @@ void Image_Mirroring(BYTE* MemIn, BYTE* MemOut)
 	{
 		for (int j = 0; j < width; j++)
 		{
-			MemOut[i * j] = MemIn[i * width - j];
+			MemOut[i * width + j] = MemIn[(i * width) + (width - j)];
 		}
 	}
-	
+
 }
 
 void Image_Flipping(BYTE* MemIn, BYTE* MemOut)
@@ -113,7 +113,7 @@ void Image_Flipping(BYTE* MemIn, BYTE* MemOut)
 	{
 		for (int j = 0; j < width; j++)
 		{
-			MemOut[i * j] = MemIn[height - i * j];
+			MemOut[i * width + j] = MemIn[((height - i) * width) + j];
 		}
 	}
 }
@@ -124,7 +124,7 @@ void Image_Reducing(BYTE* MemIn, BYTE* MemOut, int size_factor)
 	{
 		for (int j = 0; j < width / size_factor; j++)
 		{
-			MemOut[i * j] = MemIn[(i * size_factor) * (j * size_factor)];
+			MemOut[(i * width) + j] = MemIn[size_factor * ((i * width) + j)];
 		}
 	}
 
@@ -132,35 +132,43 @@ void Image_Reducing(BYTE* MemIn, BYTE* MemOut, int size_factor)
 
 void Nearest_Neighbor(BYTE* MemIn, BYTE* MemOut, int size_factor)
 {
-	for (int i = 0; i < height*size_factor; i++)
+	for (int i = 0; i < height * size_factor; i++)
 	{
-		for (int j = 0; j < width*size_factor; j++)
+		for (int j = 0; j < width * size_factor; j++)
 		{
-			MemOut[i * j] = MemIn[(i/size_factor) * (j/size_factor)];
+			if (i % size_factor == 0) {
+				MemOut[((i * width * size_factor) + j)] = MemIn[((i * width) + j) / size_factor];
+			}
+			else {
+				MemOut[((i * width * size_factor) + j)] = MemIn[(((i - (i % size_factor)) * width) + j) / size_factor];
+			}
 		}
 	}
 }
 
 void Bilinear_Interpolation(BYTE* MemIn, BYTE* MemOut, int size_factor)
 {
-	for (int i = 0; i < height*size_factor; i++)
+	for (int i = 0; i < height * size_factor; i++)
 	{
-		for (int j = 0; j < width*size_factor; j++)
+		for (int j = 0; j < width * size_factor; j++)
 		{
-			int a = (int)(j / size_factor);
-			int b = (int)(i / size_factor);
+			int a1 = j / size_factor;
+			int a2 = (j / size_factor) + 1;
+			int b1 = (i / size_factor) * width;
+			int b2 = ((i / size_factor) + 1) * width;
 
-			double x1 = (double)j / (double)size_factor - (double)a;
-			double x2 = 1 - x1;
-			double y1 = (double)i / (double)size_factor - (double)b;
-			double y2 = 1 - y1;
+			double x1 = ((double)j / (double)size_factor) - (double)a1;
+			double x2 = 1.0 - x1;
+			double y1 = ((double)i * (double)width / (double)size_factor) - (double)b1;
+			double y2 = 1.0 - y1;
 
-			double sq1 = x2 * y2;
-			double sq2 = x1 * y2;
-			double sq3 = x2 * y1;
-			double sq4 = x1 * y1;
+			double sqr1 = x2 * y2;
+			double sqr2 = x1 * y2;
+			double sqr3 = x2 * y1;
+			double sqr4 = x1 * y1;
 
-			MemOut[i * j] = sq1 * MemIn[a, b] + sq2 * MemIn[a+1, b] + sq3 * MemIn[a, b+1] + sq4 * MemIn[a+1, b+1];
+			MemOut[(i * width * size_factor) + j] = sqr1 * MemIn[a1 + b1] + sqr2 * MemIn[a2 + b1] + sqr3 * MemIn[a1 + b2] + sqr4 * MemIn[a2 + b2];
+
 		}
 	}
 }
